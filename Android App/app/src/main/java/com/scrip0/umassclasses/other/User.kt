77474aeeff15bd2c1cb8a922e053d89312610185
nsantos19 <n.santos2619@gmail.com>
2022-11-12 19:36:40 -0500

    added firebase registration

---
+++ Android App/app/src/main/java/com/scrip0/umassclasses/other/User.kt
@@ -0,0 +1,4 @@
+package com.scrip0.umassclasses.other
+
+class User {
+}
---
+++ Android App/app/src/main/java/com/scrip0/umassclasses/ui/fragments/RegisterUserFragment.kt
@@ -0,0 +1,25 @@
+package com.scrip0.umassclasses
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+
+class RegisterUser : Fragment() {
+
+    var editTextPassword : EditText = 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register_user, container, false)
+    }
+
+}
